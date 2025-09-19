ba6a9e40dd72f62a4965b385b035d99285b72994
Md Moniruzzaman <mdmoniruzzamanshuvo2003@gmail.com>
2025-09-20 00:12:30 +0600

    service page issue solved

--- app/services-details/page.tsx
+++ app/services-details/page.tsx
@@ -7,12 +7,13 @@ import VideoContentSection from "@/components/ui/services-details/VideoContentSe
 
 import { servicesData } from "@/data/servicesData"; // ✅ Import your data
 
-export default function ServiceDetailspage({
-  searchParams,
-}: {
-  searchParams: { category?: string };
-}) {
-  const selectedCategory = searchParams.category || "Web Development & Design";
+interface PageProps {
+  searchParams: Promise<{ category?: string }>;
+}
+
+export default async function ServiceDetailspage({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const selectedCategory = params.category || "Web Development & Design";
 
   const videoUrl =
     "https://pro-bucket.s3.us-east-1.amazonaws.com/1758160755855_serviecs-details-for-ai-intregrated+web+and+moblie+web.mp4";
