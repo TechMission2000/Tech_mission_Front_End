992308074212a4616fa0e0a5e1fd8e2674267ee0
Mehedi Hasan <mehedihasan@Mehedis-Mac-mini.local>
2025-10-02 12:00:55 +0600

    some linkup added in header footer an others buttons

--- components/about/OurProcess.tsx
+++ components/about/OurProcess.tsx
@@ -28,28 +28,28 @@ function OurProcess() {
           {/* Right Content - Process Flow */}
           <div className="relative ">
             <svg width="1010" height="447" viewBox="0 0 1010 447" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_684_9303)">
-<path d="M25 310.797C70.3168 345.797 175.867 411.797 235.534 395.797C310.118 375.797 328.528 272.797 443.708 248.797C558.888 224.797 643.385 308.297 732.602 187.797C821.82 67.2971 865.248 -11.7029 985.149 5.29715" stroke="#195296" stroke-opacity="0.95" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M25 310.797C70.3168 345.797 175.867 411.797 235.534 395.797C310.118 375.797 328.528 272.797 443.708 248.797C558.888 224.797 643.385 308.297 732.602 187.797C821.82 67.2971 865.248 -11.7029 985.149 5.29715" stroke="#1963B9" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
-</g>
-<defs>
-<filter id="filter0_d_684_9303" x="0.5" y="0.5" width="1009.15" height="446.256" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dy="24"/>
-<feGaussianBlur stdDeviation="11"/>
-<feComposite in2="hardAlpha" operator="out"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0.215686 0 0 0 0 0.203922 0 0 0 0 0.662745 0 0 0 0.3 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_684_9303"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_684_9303" result="shape"/>
-</filter>
-</defs>
-</svg>
+              <g filter="url(#filter0_d_684_9303)">
+                <path d="M25 310.797C70.3168 345.797 175.867 411.797 235.534 395.797C310.118 375.797 328.528 272.797 443.708 248.797C558.888 224.797 643.385 308.297 732.602 187.797C821.82 67.2971 865.248 -11.7029 985.149 5.29715" stroke="#195296" stroke-opacity="0.95" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M25 310.797C70.3168 345.797 175.867 411.797 235.534 395.797C310.118 375.797 328.528 272.797 443.708 248.797C558.888 224.797 643.385 308.297 732.602 187.797C821.82 67.2971 865.248 -11.7029 985.149 5.29715" stroke="#1963B9" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" />
+              </g>
+              <defs>
+                <filter id="filter0_d_684_9303" x="0.5" y="0.5" width="1009.15" height="446.256" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                  <feOffset dy="24" />
+                  <feGaussianBlur stdDeviation="11" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix type="matrix" values="0 0 0 0 0.215686 0 0 0 0 0.203922 0 0 0 0 0.662745 0 0 0 0.3 0" />
+                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_684_9303" />
+                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_684_9303" result="shape" />
+                </filter>
+              </defs>
+            </svg>
 
 
             <div className="">
               {/* Card 1 - Bottom Left */}
-              <div 
+              <div
                 className="absolute w-64 md:w-72"
                 // Positioned below the first dot (cy=580)
                 style={{ top: 'calc(96.6% + 25px)', left: '4%' }}
@@ -65,7 +65,7 @@ function OurProcess() {
               </div>
 
               {/* Card 2 - Middle */}
-              <div 
+              <div
                 className="absolute w-64 md:w-72"
                 // Positioned below the second dot (cy=350)
                 style={{ top: 'calc(58.3% + 25px)', left: '50%', transform: 'translateX(-50%)' }}
@@ -82,7 +82,7 @@ function OurProcess() {
               </div>
 
               {/* Card 3 - Top Right */}
-              <div 
+              <div
                 className="absolute w-64 md:w-72"
                 // Positioned below the third dot (cy=100)
                 style={{ top: 'calc(16.7% + 25px)', left: '96%', transform: 'translateX(-100%)' }}
