fc59a6a3c9d2cf1b5d884b1eacfbba0869612f3f
AsifurRhman <khansourav58@gmail.com>
2025-10-10 15:25:29 +0600

    edit

---
+++ app/(main)/about/page.tsx
@@ -0,0 +1,27 @@
+import AboutHero from '@/components/about/AboutHero';
+import GetIntouch from '@/components/about/GetIntouch';
+import OurMission from '@/components/about/Ourmission';
+import OurProcess from '@/components/about/OurProcess';
+import OurStory from '@/components/about/OurStory';
+import { AboutUs } from '@/components/ui/landing/about-us';
+import Testimonials from '@/components/ui/landing/Testimonials';
+import WhyChooseUs from '@/components/ui/landing/WhyChooseUs';
+import Team from '@/components/ui/service/Team';
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <AboutHero/>
+      <AboutUs/>
+      <OurMission/>
+      <OurStory/>
+      <WhyChooseUs/>
+      {/* <Team/> */}
+      <Testimonials/>
+      {/* <OurProcess/> */}
+      <GetIntouch/>
+
+    </div>
+  );
+}
